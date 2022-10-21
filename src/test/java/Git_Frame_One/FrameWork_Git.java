139826f9f0180fa4bb3034d9acab72ded7f4ee93
HemanthkumarBHK97 <Hemanth.bhk97@gmail.com>
2022-10-21 19:09:22 +0530

    Commited the Frame_One

---
+++ src/test/java/Git_Frame_One/FrameWork_Git.java
@@ -0,0 +1,19 @@
+package Git_Frame_One;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class FrameWork_Git {
+	
+	@Test
+	public void Frame() {
+
+		String key ="webdriver.chrome.driver";
+		String value="./chromedriver.exe";
+		System.setProperty(key, value);
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8888");	
+	}
+}
